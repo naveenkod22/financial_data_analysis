9dcf345192f5b94a5094d9fe00a4867711e0e04b
naveenkod22 <naveenkod22@gmail.com>
2023-02-02 20:16:32 -0500

    updated with Feb 02 2023 Data and formated Date column,rounded all numeric columns to 4 decimal points for all files in watch list folder

--- scripts/get_data.py
+++ scripts/get_data.py
@@ -4,43 +4,43 @@ if datetime.datetime.today().weekday() in [0, 1, 2, 3, 4]:
     import yfinance as yf
     from yahoo_fin import stock_info as si
 
-    now = str(datetime.datetime.now())
-    now = now[0:16].replace("-", "").replace(" ", "_").replace(":","")
+    # now = str(datetime.datetime.now())
+    # now = now[0:16].replace("-", "").replace(" ", "_").replace(":","")
 
-    def convert_to_numeric(x):
-        try:
-            return pd.to_numeric(x)
-        except ValueError:
-            if x.endswith("T"):
-                return float(x[:-1]) * 1e12
-            elif x.endswith("B"):
-                return float(x[:-1]) * 1e9
-            elif x.endswith("M"):
-                return float(x[:-1]) * 1e6
+    # def convert_to_numeric(x):
+    #     try:
+    #         return pd.to_numeric(x)
+    #     except ValueError:
+    #         if x.endswith("T"):
+    #             return float(x[:-1]) * 1e12
+    #         elif x.endswith("B"):
+    #             return float(x[:-1]) * 1e9
+    #         elif x.endswith("M"):
+    #             return float(x[:-1]) * 1e6
 
-    file_name = './data/active/active'+now+'.csv'
-    day_active = si.get_day_most_active(count = 200)
-    day_active["Market Cap"] = day_active["Market Cap"].apply(convert_to_numeric)
-    day_active['Market Cap'] = day_active['Market Cap']/1000000000
-    day_active.rename(columns={"Market Cap" : "Market Cap(Billions)"}, inplace=True)
-    day_active.to_csv(file_name, index=False)
+    # file_name = './data/active/active'+now+'.csv'
+    # day_active = si.get_day_most_active(count = 200)
+    # day_active["Market Cap"] = day_active["Market Cap"].apply(convert_to_numeric)
+    # day_active['Market Cap'] = day_active['Market Cap']/1000000000
+    # day_active.rename(columns={"Market Cap" : "Market Cap(Billions)"}, inplace=True)
+    # day_active.to_csv(file_name, index=False)
 
-    file_name = './data/losers/losers'+now+'.csv'
-    day_losers = si.get_day_losers(count = 200)
-    day_losers["Market Cap"] = day_losers["Market Cap"].apply(convert_to_numeric)
-    day_losers['Market Cap'] = day_losers['Market Cap']/1000000000
-    day_losers.rename(columns={"Market Cap" : "Market Cap(Billions)"}, inplace=True)
-    day_losers.to_csv(file_name, index=False)
+    # file_name = './data/losers/losers'+now+'.csv'
+    # day_losers = si.get_day_losers(count = 200)
+    # day_losers["Market Cap"] = day_losers["Market Cap"].apply(convert_to_numeric)
+    # day_losers['Market Cap'] = day_losers['Market Cap']/1000000000
+    # day_losers.rename(columns={"Market Cap" : "Market Cap(Billions)"}, inplace=True)
+    # day_losers.to_csv(file_name, index=False)
 
-    file_name = './data/gainers/gainers_'+now+'.csv'
-    day_gainers = si.get_day_gainers(count = 200)
-    day_gainers["Market Cap"] = day_gainers["Market Cap"].apply(convert_to_numeric)
-    day_gainers['Market Cap'] = day_gainers['Market Cap']/1000000000
-    day_gainers.rename(columns={"Market Cap" : "Market Cap(Billions)"}, inplace=True)
-    day_gainers.to_csv(file_name, index=False)
+    # file_name = './data/gainers/gainers_'+now+'.csv'
+    # day_gainers = si.get_day_gainers(count = 200)
+    # day_gainers["Market Cap"] = day_gainers["Market Cap"].apply(convert_to_numeric)
+    # day_gainers['Market Cap'] = day_gainers['Market Cap']/1000000000
+    # day_gainers.rename(columns={"Market Cap" : "Market Cap(Billions)"}, inplace=True)
+    # day_gainers.to_csv(file_name, index=False)
 
 
-    start = datetime.datetime.today() - datetime.timedelta(2)
+    start = datetime.datetime.today() - datetime.timedelta(1)
     to = datetime.datetime.today()
 
     tech = ["AAPL", "MSFT", "GOOG", "AMZN", "META","BABA", "ASML", "AVGO", "ORCL", "ADBE", "TXN"]
@@ -55,6 +55,6 @@ if datetime.datetime.today().weekday() in [0, 1, 2, 3, 4]:
             temp = yf.download(ticker, start=start, end=to, interval = "1d", actions = True)
             temp = temp.reset_index()
             temp2 = pd.read_csv("./data/watchlist/"+ticker+".csv")
-            temp2 = pd.concat([temp2, temp], axis=0, ignore_index=False)
-            temp2.reset_index()
-            temp2.to_csv("./data/watchlist/"+ticker+".csv", index = False)
+            df = pd.concat([temp2, temp], axis=0, ignore_index=False)
+            df.reset_index()
+            df.to_csv("./data/watchlist/"+ticker+".csv", index = False)
